8c104c45e64a426a4789a4195e82fcf97ffcfbca
MANTHAN SRIVASTAV <91532375+Manthansrivastav1234@users.noreply.github.com>
2022-06-28 22:33:31 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,96 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  userClickedPattern.push(this.id);
+  animatePress(this.id);
+
+  //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+  checkAnswer(userClickedPattern.length-1);
+});
+
+
+//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+function checkAnswer(currentLevel) {
+
+//at every level we are checking the whole pattern is matching or not
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //then when the whole pattern matches then we execute next sequence
+      if (userClickedPattern.length === gamePattern.length){
+
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+      var audio1=new Audio("sounds/wrong.mp3");
+      audio1.play();
+      $("body").addClass("game-over");
+      setTimeout(function()
+    {
+        $("body").removeClass("game-over");
+        $("h1").text("Game Over, Press Any Key to Restart")
+
+    },200);
+    startOver();
+
+    }
+
+}
+
+function nextSequence() {
+
+  //. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+function startOver()
+{
+  level=0;
+  gamePattern=[];
+  started=false;
+}
